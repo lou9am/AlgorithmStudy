70e6d671f992acd35e7f4fe9fd77364edf8d0520
lou <118880696+lou9am@users.noreply.github.com>
2023-04-28 15:53:18 +0900

    Create Prefix_sum.py
    prefix sum

---
+++ notes/Prefix_sum.py
@@ -0,0 +1,14 @@
+# 구간 합 계산 Prefix_sum
+# left 번째에서 right 번째까지 구간합을 알고 싶을 때
+n = 5
+data = [10, 20, 30, 40, 50]
+
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left-1]) 
